4a6702f2ce784309fe3f73a79a4fc988bb08d967
Anshul Kumar <84836148+ianshulx@users.noreply.github.com>
2022-10-10 16:02:38 +0530

    Update settings.py

--- TSFBank/settings.py
+++ TSFBank/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'zopvw3^bl^qg$eumc48ujhg!!mplt7_a3p=vw)tp&y9@ev%rmf'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-	'banking-management-system-tsf.herokuapp.com',
+	'tsf-banking-app.herokuapp.com',
 	'127.0.0.1'
 ]
 
